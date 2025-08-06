ed162caf95189f1eae4304dfe5bdaf61312e45e2
tribhuwanpandey <tribhuwanpandey733@gmail.com>
2025-08-06 15:09:50 +0530

    Variables in Bash-Scripting

---
+++ Variables/Variable-demo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Use logs from a central folder
+LOG_DIR="D:\AWS-DevOps\Bash-Scripting\logs"
+APP_LOG_FILE="Application.log"
+SYS_LOG_FILE="System.log"
+
+echo " analysing log files "
+echo "==================="
+
+echo -e "\nList of log files updated in last 24H"
+find $LOG_DIR -name "*.log" -mtime -1
+
+echo -e "\nsearching WARNING logs in application.log file"
+grep "WARNING" "$LOG_DIR/$APP_LOG_FILE"
+ 
+echo -e "\nNumber of WARNING logs found in application.log"
+grep -c "WARNING" "$LOG_DIR/$APP_LOG_FILE"
+
+echo -e "\nNumber of INFO logs found in application.log"
+grep -c "INFO" "$LOG_DIR/$APP_LOG_FILE"
+
+echo -e"\nsearching FATAL logs in system.log file"
+grep "FATAL" "$LOG_DIR/$SYS_LOG_FILE" 
+
+echo -e"\nNumber of FATAL logs in system.log file" 
+grep -c "FATAL" "$LOG_DIR/$SYS_LOG_FILE"
+
+echo -e"\nNumber of CRITICAL logs in system.log file"
+grep -c "CRITICAL" "$LOG_DIR/$SYS_LOG_FILE"
